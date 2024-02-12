fb833904cdbf911c619f11ee5740f11b967e54d0
sergeyvna <insedova@mail.com>
2024-02-12 20:19:53 +0000

    Added scrolling gallery

--- src/Pages/Home/Gallery.jsx
+++ src/Pages/Home/Gallery.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from 'react';
-
 export default function Gallery() {
     return (
-        <section>
-            <div className="gallery-content">
-                <h2 className="title">Gallery</h2>
-                <div className="gallery-slider">
-                    <div className="slide-track">
-                        <div className="slide">
-                            <img src="./img/wheel-icon.png" alt="Wheel" />
-                        </div>
-                    </div>
+        <section className="logos-section">
+             <h2 className="title">Gallery</h2>
+            <div className='logos'>
+                <div className='logos-slide'>
+                    <img src='./img/gallery-img-1.jpg' alt="gallery image 1"/>
+                    <img src='./img/gallery-img-2.jpg' alt="gallery image 2"/>
+                    <img src='./img/gallery-img-3.jpg' alt="gallery image 3"/>
+                    <img src='./img/gallery-img-4.jpg' alt="gallery image 4"/>
+                    <img src='./img/gallery-img-5.jpg' alt="gallery image 5"/>
+                    <img src='./img/gallery-img-6.jpg' alt="gallery image 6"/>
+                    <img src='./img/gallery-img-7.jpg' alt="gallery image 7"/>
+                    <img src='./img/gallery-img-8.jpg' alt="gallery image 8"/>
+                </div>
+
+                <div className='logos-slide'>
+                    <img src='./img/gallery-img-1.jpg' alt="gallery image 1"/>
+                    <img src='./img/gallery-img-2.jpg' alt="gallery image 2"/>
+                    <img src='./img/gallery-img-3.jpg' alt="gallery image 3"/>
+                    <img src='./img/gallery-img-4.jpg' alt="gallery image 4"/>
+                    <img src='./img/gallery-img-5.jpg' alt="gallery image 5"/>
+                    <img src='./img/gallery-img-6.jpg' alt="gallery image 6"/>
+                    <img src='./img/gallery-img-7.jpg' alt="gallery image 7"/>
+                    <img src='./img/gallery-img-8.jpg' alt="gallery image 8"/>
                 </div>
             </div>
         </section>
