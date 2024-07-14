a5742c36eb0a95ed0447bc52a1b1e78725aae60f
sergeyvna <insedova@mail.com>
2024-07-14 12:05:24 +0100

    new emailjs form submission

--- src/Pages/Home/ContactForm.jsx
+++ src/Pages/Home/ContactForm.jsx
@@ -1,102 +1,95 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 export default function ContactForm() {
     const [activeTab, setActiveTab] = useState(0);
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
     const handleTabClick = (index) => {
         setActiveTab(index);
     };
-
-    const [isSubmitting, setIsSubmitting] = useState(false);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         setIsSubmitting(true);
 
         const form = event.target;
-        const formData = new FormData(form);
 
         try {
-            // Perform form submission asynchronously
-            await fetch(form.action, {
-                method: form.method,
-                body: formData,
-            });
+            // Perform form submission using EmailJS
+            await emailjs.sendForm('service_oyxhm3z', 'template_aoadi0m', form, '8DDhqFAXNqaZjlCG-');
 
             // Redirect to the success page
             window.location.href = form.getAttribute('data-success-url');
         } catch (error) {
             console.error('Form submission error:', error);
             // Handle any errors here
+        } finally {
+            setIsSubmitting(false);
         }
     };
-    
+
     return (
         <section id='contact'>
-           <div className='parent'>
-            <div className="title-container">
-                <h2 className="title">Get a Personalized Quote</h2>
-            </div>
-            <div className="tab-container">
-            
-                <div className="tab-box">
-                    <button className={`tab-btn ${activeTab === 0 ? 'active' : ''}`} onClick={() => handleTabClick(0)}>General</button>
-                    <button className={`tab-btn ${activeTab === 1 ? 'active' : ''}`} onClick={() => handleTabClick(1)}>Tyres</button>
-                    <div className="line" style={{ left: activeTab === 0 ? '0%' : '50%' }}></div>
+            <div className='parent'>
+                <div className="title-container">
+                    <h2 className="title">Get a Personalized Quote</h2>
                 </div>
-
-                <div className="tab-content-box">
-                    <div className="tab-content" style={{ display: activeTab === 0 ? 'block' : 'none' }}>
-                        <form  onSubmit={handleSubmit} action="https://formsubmit.co/f0a7b6a729bd7f53ff95e224651bec94" method="POST" className="contact-form-container" data-success-url="https://pitstopautos.netlify.app/success">
-                            
-                            <input type="text" name="_honey" style={{display: "none"}}/>
-                            <input type="hidden" name="_captcha" value="false"/>
-                            {/*<input type="hidden" name="_next" value="https://pitstopautos.netlify.app/success"/>*/}
-
-                            <div className="contact-container">
-                                <label htmlFor='Full name' className='contact-label'>
-                                    <span>Full Name</span>
-                                    <input type="text" className="contact-input" name="Full&nbsp;Name" id="full-name" required />
-                                </label>
-
-                                <label htmlFor='Email' className='contact-label'>
-                                    <span>Email</span>
-                                    <input type="email" className="contact-input" name="Email" id="email" required  placeholder="jsmith@gmail.com"/>
-                                 </label>
-
-                                <label htmlFor='Phone' className='contact-label'>
-                                    <span>Phone</span>
-                                    <input type="text" className="contact-input" name="Phone" id="phone" required />
-                                </label>
-
-                                <label htmlFor='Car Registration' className='contact-label'>
-                                    <span>Car Registration</span>
-                                    <input type="text" className="contact-input" name="Car&nbsp;Registration" id="car-model" required placeholder="OV21MTU"/>
-                                </label>
-
-                                <label htmlFor='Message' className='contact-label'>
-                                    <span>Message</span>
-                                    <textarea className="contact-input" id="message" name="Message" rows="3" placeholder="Service Request" maxLength={250} required/>
-                                </label>
-
-                                <label className='checkbox-label'>
-                                    <input type="checkbox" required id="checkbox"/>
-                                    <span>I agree to terms & conditions provided by the company. By providing my details, I agree to receive email messages from the business.</span>
-                                </label>
-
-                                <div>
-                                    <button type="submit" className={`btn btn-primary contact-form-btn ${isSubmitting ? 'submitting' : ''}`} disabled={isSubmitting}>
-                                        <span>{isSubmitting ? 'Submitting...' : 'Submit'}</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-
-                    
+                <div className="tab-container">
+                    <div className="tab-box">
+                        <button className={`tab-btn ${activeTab === 0 ? 'active' : ''}`} onClick={() => handleTabClick(0)}>General</button>
+                        <button className={`tab-btn ${activeTab === 1 ? 'active' : ''}`} onClick={() => handleTabClick(1)}>Tyres</button>
+                        <div className="line" style={{ left: activeTab === 0 ? '0%' : '50%' }}></div>
                     </div>
 
+                    <div className="tab-content-box">
+                        <div className="tab-content" style={{ display: activeTab === 0 ? 'block' : 'none' }}>
+                            <form onSubmit={handleSubmit} className="contact-form-container" data-success-url="http://localhost:3000/success">
+                                <input type="text" name="_honey" style={{display: "none"}}/>
+                                <input type="hidden" name="_captcha" value="false"/>
+
+                                <div className="contact-container">
+                                    <label htmlFor='full-name' className='contact-label'>
+                                        <span>Full Name</span>
+                                        <input type="text" className="contact-input" name="full_name" id="full-name" required />
+                                    </label>
+
+                                    <label htmlFor='email' className='contact-label'>
+                                        <span>Email</span>
+                                        <input type="email" className="contact-input" name="email" id="email" required  placeholder="jsmith@gmail.com"/>
+                                    </label>
+
+                                    <label htmlFor='phone' className='contact-label'>
+                                        <span>Phone</span>
+                                        <input type="text" className="contact-input" name="phone" id="phone" required />
+                                    </label>
+
+                                    <label htmlFor='car-reg' className='contact-label'>
+                                        <span>Car Registration</span>
+                                        <input type="text" className="contact-input" name="car_reg" id="car-reg" required placeholder="OV21MTU"/>
+                                    </label>
+
+                                    <label htmlFor='message' className='contact-label'>
+                                        <span>Message</span>
+                                        <textarea className="contact-input" id="message" name="message" rows="3" placeholder="Service Request" maxLength={250} required/>
+                                    </label>
+
+                                    <label className='checkbox-label'>
+                                        <input type="checkbox" required id="checkbox"/>
+                                        <span>I agree to terms & conditions provided by the company. By providing my details, I agree to receive email messages from the business.</span>
+                                    </label>
+
+                                    <div>
+                                        <button type="submit" className={`btn btn-primary contact-form-btn ${isSubmitting ? 'submitting' : ''}`} disabled={isSubmitting}>
+                                            <span>{isSubmitting ? 'Submitting...' : 'Submit'}</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
                     <div className="tab-content" style={{ display: activeTab === 1 ? 'block' : 'none' }}>
-                    <form  onSubmit={handleSubmit} action="https://formsubmit.co/f0a7b6a729bd7f53ff95e224651bec94" method="POST" className="contact-form-container" data-success-url="https://pitstopautos.netlify.app/success">
+                    <form  onSubmit={handleSubmit} action="https://formsubmit.co/f0a7b6a729bd7f53ff95e224651bec94" method="POST" className="contact-form-container" data-success-url="http://localhost:3000/success">
                             
                             <input type="text" name="_honey" style={{display: "none"}}/>
                             <input type="hidden" name="_captcha" value="false"/>
